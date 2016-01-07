70c5174a1bbcc7a7016d2fa9a379236524061102
Alban Paczkowski <true>
2016-01-07 21:51:41 +0100

    Work on ListView

--- src/com/example/ProjetAndroidESGI/MyActivity.java
+++ src/com/example/ProjetAndroidESGI/MyActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.ProjetAndroidESGI.Adapter.GridListAdapter;
 
 public class MyActivity extends ListActivity {
     /**
@@ -15,6 +18,9 @@ public class MyActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new GridListAdapter());
 
         Intent intentWeb = new Intent(this, ArticleWeb.class);
 
